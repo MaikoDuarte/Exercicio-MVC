de471da929528749ca55721e7d6c3b2aa2564dbc
Maiko Duarte <mkdu185@gmail.com>
2023-11-07 22:02:03 -0300

    Initial commit

---
+++ WebApplication1/Controllers/LanchoneteController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.Controllers;
+
+public class LanchoneteController : Controller
+{
+    // GET
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public IActionResult Pastel()
+    {
+        return Pastel();
+    }
+
+    public IActionResult Coxinha()
+    {
+        return Coxinha();
+    }
+    
+    
+}
